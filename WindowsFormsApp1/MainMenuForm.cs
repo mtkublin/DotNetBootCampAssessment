31881caac79a99c04d151918415f7d6e50cdd465
mtkublin <mtkublin@gmail.com>
2019-03-25 23:56:13 +0100

    Raports a to e more or less ready

--- WindowsFormsApp1/MainMenuForm.cs
+++ WindowsFormsApp1/MainMenuForm.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Linq;
 using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,10 +55,15 @@ namespace ReqRaportsApp
 
         public void ReqQuantBtn_Click(object sender, EventArgs e)
         {
-            int allReqsCount = RequestsList.Count;
+            Dictionary<string, List<int>> allReqDict = AllRequests();
+
+            int allReqsCount = 0;
+            foreach (string k in allReqDict.Keys)
+            {
+                allReqsCount += allReqDict[k].Count;
+            }
 
             DialogResult res = MessageBox.Show("Ilość zamówień: " + allReqsCount.ToString() + "\nCzy chcesz zapisać raport?", "Ilość zamówień", MessageBoxButtons.OKCancel);
-
             if (res == DialogResult.OK)
             {
                 MessageBox.Show("Zapisano raport");
@@ -66,22 +72,90 @@ namespace ReqRaportsApp
 
         public void ReqQuantForClientIdBtn_Click(object sender, EventArgs e)
         {
+            string currentClientId = "1";
 
+            IEnumerable<int> getClientReqs = from request in RequestsList
+                                where request.clientId == currentClientId
+                                select request.requestId;
+
+            HashSet<int> reqIdsForClient = new HashSet<int>(getClientReqs.ToList());
+
+            int clientReqsCount = reqIdsForClient.Count();
+
+            DialogResult res = MessageBox.Show("Ilość zamówień dla klienta o identyfikatorze " + currentClientId + ": " + clientReqsCount.ToString() + "\nCzy chcesz zapisać raport?", "Ilość zamówień dla klienta", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                MessageBox.Show("Zapisano raport");
+            }
         }
 
         public void ReqValueSumBtn_Click(object sender, EventArgs e)
         {
+            double allReqsValueSum = 0;
 
+            foreach (request r in RequestsList)
+            {
+                int quant = r.quantity;
+                double price = r.price;
+                double val = quant * price;
+
+                allReqsValueSum += val;
+            }
+
+            DialogResult res = MessageBox.Show("Łączna kwota zamówień: " + allReqsValueSum.ToString() + "\nCzy chcesz zapisać raport?", "Łączna kwota zamówień", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                MessageBox.Show("Zapisano raport");
+            }
         }
 
         public void ReqValueSumForClientIdBtn_Click(object sender, EventArgs e)
         {
+            string currentClientId = "1";
 
+            var getClientReqs = from request in RequestsList
+                                where request.clientId == currentClientId
+                                select request;
+
+            List<request> clientReqs = getClientReqs.ToList();
+
+            double clientReqsValueSum = 0;
+
+            foreach (request r in clientReqs)
+            {
+                int quant = r.quantity;
+                double price = r.price;
+                double val = quant * price;
+
+                clientReqsValueSum += val;
+            }
+
+            DialogResult res = MessageBox.Show("Łączna kwota zamówień dla klienta o identyfikatorze " + currentClientId + ": " + clientReqsValueSum.ToString() + "\nCzy chcesz zapisać raport?", "Łączna kwota zamówień dla klienta", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                MessageBox.Show("Zapisano raport");
+            }
         }
 
         public void AllReqsListBtn_Click(object sender, EventArgs e)
         {
+            Dictionary<string, List<int>> allReqDict = AllRequests();
+            string reqsListString = string.Empty;
 
+            foreach (string k in allReqDict.Keys)
+            {
+                reqsListString += k + ":\n";
+                foreach (int rid in allReqDict[k])
+                {
+                    reqsListString += "    - " + rid.ToString() + "\n";
+                }
+            }
+
+            DialogResult res = MessageBox.Show(reqsListString + "\nCzy chcesz zapisać raport?", "Łączna kwota zamówień dla klienta", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                MessageBox.Show("Zapisano raport");
+            }
         }
 
         public void ReqsListForClientIdBtn_Click(object sender, EventArgs e)
---
+++ WindowsFormsApp1/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReqRaportsApp
+{
+    public partial class Form1
+    {
+        public Dictionary<string, List<int>> AllRequests()
+        {
+            IEnumerable<string> getClientIds = from request in RequestsList
+                                               select request.clientId;
+
+            HashSet<string> clientIds = getClientIds.ToHashSet();
+
+            Dictionary<string, List<int>> allReqsDict = new Dictionary<string, List<int>>();
+            foreach (string cid in clientIds)
+            {
+                IEnumerable<int> getClientsReqIds = from request in RequestsList
+                                                    where request.clientId == cid
+                                                    select request.requestId;
+
+                HashSet<int> reqIds = getClientsReqIds.ToHashSet();
+
+                allReqsDict[cid] = reqIds.ToList();
+            }
+
+            return allReqsDict;
+        }
+    }
+}
